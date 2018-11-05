48a3e91b67f421b828f79e881bb0f20f822cb997
Matt Ludwigs <mattludwigs@gmail.com>
2018-11-05 10:43:30 -0700

    Add ex_docs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Elixircom.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -21,7 +23,26 @@ defmodule Elixircom.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nerves_uart, "~> 1.2"}
+      {:nerves_uart, "~> 1.2"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      extras: [
+        "README.md"
+      ],
+      source_url: "https://github.com/mattludwigs/elixircom"
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Matt Ludwigs"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/mattludwigs/elixircom"}
     ]
   end
 end
